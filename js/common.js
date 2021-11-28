9502e360ebeab82a19e128405ee66d91d8b2323b
edy03133 <84275562+edy03133@users.noreply.github.com>
2021-11-29 05:33:42 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,6 +5,13 @@ $('.slides').slick({
     autoplaySpeed:3000,// スライドスピード
     speed:500,// スクロールスピード
     arrows: true, // 矢印の表示
-    prevArrow:'<img src="../img/btn_prev.svg" alt="" class="prev_icon">',
-    nextArrow:'<img src="../img/btn_next.svg" alt="" class="next_icon">'
+  });
+
+$('.policy-slides').slick({
+    autoplay: true, //自動再生
+    dots: true, //ドットの表示
+    infinite:true,// 無限にスライド
+    autoplaySpeed:3000,// スライドスピード
+    speed:500,// スクロールスピード
+    arrows: true, // 矢印の表示
   });
